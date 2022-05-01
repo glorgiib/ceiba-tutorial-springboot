ebd9936d4d53ac202582f5befcccd8823c9d3423
glorgiib <glorgiib.gegi@gmail.com>
2022-05-01 18:51:03 -0500

    creacion

---
+++ src/main/java/com/glorgiib/CeibaTutorialSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.glorgiib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaTutorialSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaTutorialSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glorgiib/CeibaTutorialSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glorgiib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeibaTutorialSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
